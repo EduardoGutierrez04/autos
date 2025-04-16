d835b0ae5bf4d5ed0004e38741c627d25f58beba
EduardoGutierrez04 <edanniel25@gmail.com>
2025-04-16 10:56:49 -0600

    creacion

---
+++ src/test/java/mx/uam/integracion/autos/autos/AutosApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.integracion.autos.autos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
